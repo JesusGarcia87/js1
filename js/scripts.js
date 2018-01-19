9c48b73d1696e5b9aabaf2916ad7d07fa2422181
unknown <jesus1287garcia@yahoo.com>
2018-01-18 20:54:52 -0800

    nite too

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/*var sentence = 2;
+console.log(sentence);
+*/
+{
+
+
+
+}
+
+//let firstNumber =22; // declsrstion and definition
+//var firstNumber;// declsrstion
+//var secondNumber =22;//definition
+//var secondNumber =11;
+
+//let firstNumber =11;
+
+//console.log(secondNumber);
+
+//console.log('testing testing');
+
+/*
+{
+ const truth ="Ep8 was.... Terrible";
+ console.log(truth);
+}
+const truth ="ep8 was.... meh...";
+console.log(truth);
+*/
+/*let rightNow = new Date();
+console.log(rightNow);
+
+var ilikePi = Math.Pi;
+console.log(ilikePi);
+*/
+
+var string = "lalalalal";
+var new String = new String("lalalal");
+
+console.log(string);
+console.log(newString.touppercase());
