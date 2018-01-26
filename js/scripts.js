13ead4f4ca4bdf4b6ddbe1ad8925d5e295e2a815
unknown <jesus1287garcia@yahoo.com>
2018-01-25 19:48:26 -0800

    end of day 3

---
+++ js/scripts.js
@@ -0,0 +1,178 @@
+/*var sentence = 2;
+console.log(sentence);
+*/
+
+//let firstNumber =22; // declsrstion and definition
+//var firstNumber;// declsrstion
+//var secondNumber =22;//definition
+//var secondNumber =11;
+
+//let firstNumber =11;
+
+//console.log(secondNumber);
+
+//console.log('testing testing');
+
+/*
+{
+ const truth ="Ep8 was.... Terrible";
+ console.log(truth);
+}
+const truth ="ep8 was.... meh...";
+console.log(truth);
+*/
+/*let rightNow = new Date();
+console.log(rightNow);
+
+var ilikePi = Math.Pi;
+console.log(ilikePi);
+*/
+/*
+var string = "lalalalal";
+var new String = new String("lalalal");
+
+console.log(string);
+console.log(newString.touppercase());
+*/
+
+/*var myarray = ["Hello world",22, true,{}];
+console.log(myarray.lenght);S
+*/
+
+//var myobject ={
+//mystring: "Hello World",
+//magicNumber: 42
+//};
+
+//let helloMessage = alert("Hey, there!....");
+//let confirmMessage = confirm("is your name jesus");
+//let promtMessage = prompt("tell me the password...","password");
+//console.log(promptMessage);
+/*
+let promtMessage = prompt(What is Your first name ?...);
+console.log(promtMessage);
+let promtMessage = prompt(And last name!...)
+console.log(promtMessage);
+*/
+
+/*var firstName = prompt("what is yoour first name?","maria");
+var lastName = prompt("what is your last name?","is the best...duh");
+console.log(firstName);
+alert(lastName);
+let birthday = prompt("what is your birthday","please answer the question?..");
+confirm(new Date(birthday));*/
+
+//This is my gh-pages branch
+
+/*//this is = to let me know if thomething is equal or not == ===
+var string ="1";
+var number =1;
+console.log(string == number);
+he changes the code to fast to keep uponmy edittor but im going to
+master javascript*/
+
+/*objects is a memerie or a adress soem where oin the web
+let obj1 ={name:"jesus"};
+let obj2 ={name:"jesus"};
+console.log(obj1 === obj2);*/
+
+/*
+to check if its ture
+var primitive = "hello World!";
+var Stringobject = new String("hello world!");
+var Stringobject = new String("hello world!");
+//console.log(primitive === Stringobjec);
+var Stringobject = new String("hello world!");
+console.log(typeof(primitive));
+//console.log(typeof(Stringobject));
+*/
+/*
+var number1 = "12";
+var number2 = "780";
+number1 = new number(number1);
+number1 = new number(number2);
+parseint();
+console.log(number1 + number2);
+number1 += 12;
+console.log(number1);
+*/
+// control flow
+
+//const bestMovie = [];
+//bestMovie !== undefined ? alert('it exists!') : alert('it does\'s exists');
+/*if (typeof(bestMovie) === 'string') {
+  alert('yay its a string!');
+} else if(typeof(bestMovie) === 'number'){
+}
+  alert('you gave me a number');
+else {
+  alert("it's not a string");
+}*/
+
+
+//switch
+/*
+switch (typeof(bestMovie)) {
+  case 'string':
+    alert('yay it\'s a string!');
+    break;
+  case 'number':
+    alert('a number!');
+    break;
+  case 'boolean':
+    alert('a bool!');
+    break;
+  case 'object!':
+    alsert('an object');
+    break;
+  default:
+    alert('its an array');
+    break;
+  }*/
+//homework day 3
+  // var Age = prompt("what is your Age?","18");
+  // var  notold = prompt("you r not old enough!","sorry");
+  // console.log(Age);
+  // alert(Age);
+  // let birthday = prompt("are u at lest 80 yrs. old","please answer the question?..");
+  // confirm(new Date(birthday));
+  //
+  // if (typeof(Age) === 'string') {
+  //   alert('do u like starwars!');
+  // } else if(typeof(bestMovie) === 'number'){
+  // }
+  //   alert('you gave me a number');
+  // else {
+  //   alert("it's not a string");
+  // }
+
+var userAge = confirm('Are You over 18?','No');
+
+if(userAge == true) {
+  underEighty = confirm('Are You Under 80 yrs. old?');
+  if(underEighty)
+  var starwars = confirm("do You like Starwars?");
+  if(starwars) {
+    alert("You Rock!..");
+    }
+   } else {
+       //do something else
+     }
+   } else {
+    alert('You are not old enough..');
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
