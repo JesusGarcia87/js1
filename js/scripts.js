44fec64b358794f471262c3824888165487a1faf
unknown <jesus1287garcia@yahoo.com>
2018-01-30 20:37:18 -0800

    day 4 harder then day 3

--- js/scripts.js
+++ js/scripts.js
@@ -98,81 +98,215 @@ console.log(number1);
 */
 // control flow
 
-//const bestMovie = [];
-//bestMovie !== undefined ? alert('it exists!') : alert('it does\'s exists');
-/*if (typeof(bestMovie) === 'string') {
-  alert('yay its a string!');
-} else if(typeof(bestMovie) === 'number'){
-}
-  alert('you gave me a number');
-else {
-  alert("it's not a string");
-}*/
+// const bestMovie = [];
+// bestMovie !== undefined ? alert('it exists!') : alert('it does\'s exists');
+// if (typeof(bestMovie) === 'string') {
+//   alert('yay its a string!');
+// } else if(typeof(bestMovie) === 'number'){
+// }
+//   alert('you gave me a number');
+// else {
+//   alert("it's not a string");
+// }
+
+//
+// switch
+//  day 3 homework finsh.
+//
+// switch (typeof(bestMovie)) {
+//   case 'string':
+//     alert('yay it\'s a string!');
+//     break;
+//   case 'number':
+//     alert('a number!');
+//     break;
+//   case 'boolean':
+//     alert('a bool!');
+//     break;
+//   case 'object!':
+//     alsert('an object');
+//     break;
+//   default:
+//     alert('its an array');
+//     break;
+//   }
+
+//start homework
+
+//no good
+// var username = prompt ("whats your name?");
+// var color = prompt ("what is ur fav color?", 'blue or yellow');
+//   switch (username.toLowercase()) {
+//
+//     case blue:
+//       alert(`your blue.. ? ${username} its cool.`);
+//       break;
+//     case 'yellow':
+//     alert('is cool');
+//       break;
+//
+//     default:
+//     alert(blue is the best anyways!..)
+//     }
 
 
-//switch
-/*
-switch (typeof(bestMovie)) {
-  case 'string':
-    alert('yay it\'s a string!');
-    break;
-  case 'number':
-    alert('a number!');
-    break;
-  case 'boolean':
-    alert('a bool!');
-    break;
-  case 'object!':
-    alsert('an object');
-    break;
-  default:
-    alert('its an array');
-    break;
-  }*/
-//homework day 3
-  // var Age = prompt("what is your Age?","18");
-  // var  notold = prompt("you r not old enough!","sorry");
-  // console.log(Age);
-  // alert(Age);
-  // let birthday = prompt("are u at lest 80 yrs. old","please answer the question?..");
-  // confirm(new Date(birthday));
-  //
-  // if (typeof(Age) === 'string') {
-  //   alert('do u like starwars!');
-  // } else if(typeof(bestMovie) === 'number'){
-  // }
-  //   alert('you gave me a number');
-  // else {
-  //   alert("it's not a string");
-  // }
+// homework day 3
+//   var Age = prompt("what is your Age?","18");
+//   var  notold = prompt("you r not old enough!","sorry");
+//   console.log(Age);
+//   alert(Age);
+//   let birthday = prompt("are u at lest 80 yrs. old","please answer the question?..");
+//   confirm(new Date(birthday));
+//
+//   if (typeof(Age) === 'string') {
+//     alert('do u like starwars!');
+//   } else if(typeof(bestMovie) === 'number'){
+//   }
+//     alert('you gave me a number');
+//   else {
+//     alert("it's not a string");
+//   }
+//
+// var userAge = confirm('Are You over 18?');
+//
+// if(userAge == true) {
+//   underEighty = confirm('Are You Under 80 yrs. old?');
+//   if(underEighty)
+//   var starwars = confirm("do You like Starwars?");
+//   if(starwars) {
+//     alert("You Rock!..");
+//     }
+//   } else {
+//        //do something else
+//      }  {
+//     alert('You are not old enough..');
+//   }
 
-var userAge = confirm('Are You over 18?','No');
 
-if(userAge == true) {
-  underEighty = confirm('Are You Under 80 yrs. old?');
-  if(underEighty)
-  var starwars = confirm("do You like Starwars?");
-  if(starwars) {
-    alert("You Rock!..");
-    }
-   } else {
-       //do something else
-     }
-   } else {
-    alert('You are not old enough..');
+//array
+// var arr = ["thingone", 22, true, {}, []];
+// console.log(arr[1]);
+
+// var myArr = ["stay", 21, true, 2004];
+// console.log(myArr, myArr[4]);
+
+// console.log(typeof(myArr));
+// console.log(Array.isArray(myArr));
+// var bestteacher = prompt('who is the best teacher?','ryeker');
+// myArr.unshift(bestteacher);
+
+//splice
+// var removedItem = myArr.slice(0, 2);
+ //console.log(removedItem, myArr);
+
+// function go() {
+//    alert('hi');
+//  }
+
+
+//var myList = ['ride bike', 'wash clothes', 'do landry'['Friday', 'Saturday', 'Sundasy']];
+//var myList = ['Ride bike', 'Wash bike', 'Do landry'];
+
+// var myList = ['ride bike', 'wash clothes', 'do landry'];
+//
+// for (i=0; i < myList.length; i++) {
+//   alert('You need to'+myList[i]+ 'this weekend!.');
+// }
+//
+// myList = 0;
+// while (myList < 3) {
+//   myList++;
+// }
+//
+// console.log(n);
+
+// var text = new string;
+// texts = prompt("please enter some text.");
+//
+// alert(" i\'ll shout it out:");
+// alert(text.toUppercase());
+//
+// alert("now in lowercase...");
+// alert(txt.toLowerCase());
+//
+// alert("the first three letters are...");
+// alert(text.indexOf("a"));
+//
+// alert("the first three letters are...");
+// alert(text.substring(0, 3)),
+
+// var todolist = ['landry', 'grocery shopping', 'workout'];
+//
+// console.log(todolist.length);
+
+// var favoriteMovie = ['Star Wars VI', '1983'];
+//    var secondFavorite = ['Return of the King', '2003'];
+//    var thirdFavorite = ['Equilibrium', '2002'];
+//    var myFavoriteMovies = [];
+//    myFavoriteMovies.push(favoriteMovie, secondFavorite, thirdFavorite);
+//    //Result:
+//    [['Star Wars VI', '1983'], ['Return of the King', '2003'], ['Equilibrium', '2002']];
+//
+
+// var movieYears = [];
+// movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1],myFavoriteMovies[2][1]);
+// alert(movieYears);
+//
+var favmovie = ['starwars 1', 'steve1'];
+var favmovie2 = ['starwars 2', 'steve2'];
+var favmovie3 = ['starwars 3', 'steve3'];
+var favmovie4 = ['starwars 4', 'steve4'];
+var favmovie5 = ['starwars 5', 'steve5'];
+var myfavoritemovies = [];
+myfavoritemovies.push(favmovie, favmovie2, favmovie3, favmovie4, favmovie5);
+console.log(myfavoritemovies);
+
+
+alert(myfavoritemovies);
+console.log(favmovie2)
+
+for (var i = 0; i < myfavoritemovies.length; i++){
+  console.log(myfavoritemovies);
+  if (myfavoritemovies[i][0] == "starwars 5"){
+    alert (hey);
+    break;
   }
+}
 
 
+if(favmovie[i][0] === "star wars vi"){
+  alert
+}
 
 
+// var movie1 = ["National Treasure", "Ben Gates", 2000];
+// var movie2 = ["Pirates of the Carribean", "Jack Sparrow", 2005];
+// var movie3 = ["Mad Max", "Furiosa", 2015];
+// var movie4 = ["The Dark Knight", "Batman", 2007];
+// var movie5 = ["Serenity", "Malcolm Reynolds", 2005];
+//
+// var favoriteMovies = [];
+// favoriteMovies.push(movie1, movie2, movie3, movie4, movie5);
+//
+// for(var i = 0; i <= favoriteMovies.length - 1; i++) {
+//    if(favoriteMovies[i][0] == 'Serenity'){
+//        alert('The best movie ever made is ' + favoriteMovies[i][0] + ' where release date was ' + favoriteMovies[i][2]);
+//        break;
+//    }
+// }
+// var str = "";
+//
+// for (var i = 0; i < 9; i++) {
+//   str = str + i;
+// }
+//
+// console.log(str);
 
-
-
-
-
-
-
-
-
+// alert(favAndLeastfav);
+// let i =0;
+// while(i < favMovies.length){
+//    i++
+// }
+// for(var incrementor = 0; incrementor < favmovies.length; incrementor);
 
 //
