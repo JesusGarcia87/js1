ed2b9bf643ac5c6ffb350006444e56633400f173
unknown <jesus1287garcia@yahoo.com>
2018-02-15 18:44:41 -0800

    missed day 8

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+// // var myPtag = document.querySelector('p');
+// //
+// // console.log(myPtag.innerHTML
+// // );
+// var radio = document.querySelector("#my-radio");
+//
+//
+//
+// var h1 = document.createElement("h1");
+// var body = document.querySelector("body");
+// var p = document.createElement("p");
+//  h1.innerHTML = "Hello Tom";
+//
+//  body.appendChild(h1);
+//  p.innerHTML = "whoa!!";
+// //Appends a list of child nodes to a parent node.
+// // This is experimental, but it works in several browsers. Be aware that it could change.
+// // appendChild orginzedes in order..
+//
+// body.appendChild(h1);
+// body.appendChild(p);
+//
+// //create a new createElement
+// var newDiv = document.createElement("div");
+// // add height and color to see it
+// newDiv.style.backgroundColor = "green";
+// newDiv.style.height = "120px";
+//
+// body.insertBefore(newDiv, p);
+//
+// // console.log(body.classList);
+// // body.className = "red-background";
+// //body.classList.add("red-background");
+// //body.classList.remove("red-background");
+// var myDiv = document.createElement("div");
+//
+// myDiv.addEventListener('click', () => {
+//   body.classList.toggle('red-background');
+//   body.classList.replace("white-texts");
+// })
+//
+// body.classList.contains("white-texts")
+//
+//
+// var newImg = document.createElement("img");
+// newImg.setAttribute("src", "https://c1.staticflickr.com/8/7030/6592413485_b54b0265bf_n.jpg");
+//
+// body.appendChild(newImg);
+//
+//
+//
+//
+// \//
+//
+
+
+var myName = {
+  first: "Jesus",
+  last: "Garcia"
+};
+
+
+
+
+
+
+
+
+
+
+//
